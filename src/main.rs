654179c931cf41dec19240b69fb3cb16975d109c
Marco Melorio <marco.melorio@protonmail.com>
2021-05-01 17:01:02 +0200

    Handle gettext errors

--- src/main.rs
+++ src/main.rs
@@ -24,8 +24,10 @@ fn main() {
 
     // Prepare i18n
     setlocale(LocaleCategory::LcAll, "");
-    bindtextdomain(GETTEXT_PACKAGE, LOCALEDIR);
-    textdomain(GETTEXT_PACKAGE);
+    bindtextdomain(GETTEXT_PACKAGE, LOCALEDIR)
+        .expect("Unable to bind the text domain");
+    textdomain(GETTEXT_PACKAGE)
+        .expect("Unable to switch to the text domain");
 
     gtk::glib::set_application_name("Telegrand");
     gtk::glib::set_prgname(Some("telegrand"));
