d2f64bef874ab3593b05bafd0a73ae680e4ed404
Marco Melorio <marco.melorio@protonmail.com>
2021-09-08 18:44:42 +0200

    sidebar-chat-row: Show the sender in the last message label

--- src/session/sidebar/chat_row.rs
+++ src/session/sidebar/chat_row.rs
@@ -1,20 +1,11 @@
 use gettextrs::gettext;
-use gtk::glib;
-use gtk::prelude::*;
-use gtk::subclass::prelude::*;
-use tdgrand::enums::MessageContent;
+use gtk::{glib, prelude::*, subclass::prelude::*};
+use tdgrand::enums::{ChatType, MessageContent};
 
-use crate::session::chat::{BoxedMessageContent, Message};
+use crate::session::chat::{Message, MessageSender};
 use crate::session::components::Avatar;
 use crate::session::Chat;
 use crate::utils::escape;
-
-fn stringify_message_content(content: MessageContent) -> String {
-    match content {
-        MessageContent::MessageText(content) => escape(&content.text.text),
-        _ => format!("<i>{}</i>", gettext("This message is unsupported")),
-    }
-}
 
 mod imp {
     use super::*;
@@ -171,21 +162,28 @@ impl ChatRow {
             let timestamp_label = self_.timestamp_label.get();
             timestamp_expression.bind(&timestamp_label, "label", Some(&timestamp_label));
 
-            // Last message content
+            // Last message label
+            //
+            // FIXME: The sender name should be part of the expressions, but it can't because
+            // the sender object is an enum of two object variants and there's no way to obtain
+            // one of the two objects from a message expression to make a sender variant expression.
             let content_expression = gtk::PropertyExpression::new(
                 Message::static_type(),
                 Some(&last_message_expression),
                 "content",
             );
-            let stringified_content_expression = gtk::ClosureExpression::new(
-                move |expressions| -> String {
-                    let content = expressions[1].get::<BoxedMessageContent>().unwrap();
-                    stringify_message_content(content.0)
+            let stringified_message_expression = gtk::ClosureExpression::new(
+                move |args| {
+                    let last_message = args[1].get::<Message>().unwrap();
+                    stringify_message(last_message)
                 },
-                &[content_expression.upcast()],
+                &[
+                    last_message_expression.upcast(),
+                    content_expression.upcast(),
+                ],
             );
             let last_message_label = self_.last_message_label.get();
-            stringified_content_expression.bind(
+            stringified_message_expression.bind(
                 &last_message_label,
                 "label",
                 Some(&last_message_label),
@@ -203,23 +201,63 @@ impl ChatRow {
                 "unread-count",
             );
             let pin_visibility_expression = gtk::ClosureExpression::new(
-                move |expressions| -> bool {
-                    let is_pinned = expressions[1].get::<bool>().unwrap();
-                    let unread_count = expressions[2].get::<i32>().unwrap();
+                move |args| {
+                    let is_pinned = args[1].get::<bool>().unwrap();
+                    let unread_count = args[2].get::<i32>().unwrap();
 
                     is_pinned && unread_count <= 0
                 },
-                &[is_pinned_expression.upcast(), unread_count_expression.upcast()],
+                &[
+                    is_pinned_expression.upcast(),
+                    unread_count_expression.upcast(),
+                ],
             );
             let pin_image = self_.pin_image.get();
-            pin_visibility_expression.bind(
-                &pin_image,
-                "visible",
-                Some(&pin_image),
-            );
+            pin_visibility_expression.bind(&pin_image, "visible", Some(&pin_image));
         }
 
         self_.chat.replace(chat);
         self.notify("chat");
+    }
+}
+
+fn stringify_message(message: Message) -> String {
+    let text_content = match message.content().0 {
+        MessageContent::MessageText(content) => {
+            // The alpha value should be kept in sync with Adwaita's dim-label alpha value
+            format!("<span alpha=\"55%\">{}</span>", escape(&content.text.text))
+        }
+        _ => gettext("Unsupported message"),
+    };
+
+    let show_sender = match message.chat().type_() {
+        ChatType::BasicGroup(_) => true,
+        ChatType::Supergroup(data) => !data.is_channel,
+        ChatType::Private(_) | ChatType::Secret(_) => message.is_outgoing(),
+    };
+
+    if show_sender {
+        let sender_name = if message.is_outgoing() {
+            gettext("You")
+        } else {
+            sender_name(message.sender(), false)
+        };
+
+        format!("{}: {}", sender_name, text_content)
+    } else {
+        text_content
+    }
+}
+
+fn sender_name(sender: &MessageSender, use_full_name: bool) -> String {
+    match sender {
+        MessageSender::User(user) => {
+            if use_full_name {
+                format!("{} {}", user.first_name(), user.last_name())
+            } else {
+                user.first_name()
+            }
+        }
+        MessageSender::Chat(chat) => chat.title(),
     }
 }
