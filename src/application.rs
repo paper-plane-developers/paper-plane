8dd5c3d990aef4dc18375b2a2c8b10ae81246411
Marcus Behrendt <marcus.behrendt.86@gmail.com>
2022-06-14 00:17:20 +0200

    about-dialog: Add OSM copyright and license

--- src/application.rs
+++ src/application.rs
@@ -196,6 +196,22 @@ impl Application {
             Some(&gettext("Sponsors")),
             &["Alisson Lauffer", "Jordan Maris"],
         );
+        about.add_legal_section(
+            "Maps",
+            Some(&gettext(
+                "<span size=\"small\">Map data by \
+                <a href=\"https://www.openstreetmap.org\">OpenStreetMap</a> \
+                and contributors</span>",
+            )),
+            gtk::License::Custom,
+            Some(&gettext(
+                "OpenStreetMap® is open data, licensed under the \
+                <a href=\"https://opendatacommons.org/licenses/odbl\">\
+                Open Data Commons Open Database License </a> (ODbL) by the \
+                <a href=\"https://osmfoundation.org\">aOpenStreetMap Foundation</a> (OSMF).",
+            )),
+        );
+        about.add_acknowledgement_section(Some(&gettext("Sponsors")), &["Alisson Lauffer"]);
 
         about.present();
     }
