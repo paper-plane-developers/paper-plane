3c13f68d77fcefc91d293a7f80e4a47b3aa7a4f0
Marco Melorio <marco.melorio@protonmail.com>
2022-08-15 15:45:04 +0200

    chat: Make every property read-only
    These properties should only be changed via tdlib's updates, so it's not
    really useful to have them settable.

--- src/tdlib/chat_list.rs
+++ src/tdlib/chat_list.rs
@@ -192,7 +192,7 @@ impl ChatList {
         {
             let mut list = self.imp().list.borrow_mut();
             let chat_id = chat.id;
-            let chat = Chat::new(chat, self.session());
+            let chat = Chat::new(chat, &self.session());
 
             chat.connect_order_notify(clone!(@weak self as obj => move |_, _| {
                 obj.emit_by_name::<()>("positions-changed", &[]);
