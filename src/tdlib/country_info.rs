e7987d1599c3e07c225e1a40e081665eb02afaff
Marco Melorio <marco.melorio@protonmail.com>
2022-08-15 18:54:03 +0200

    country-info: Make every property read-only

--- src/tdlib/country_info.rs
+++ src/tdlib/country_info.rs
@@ -53,46 +53,25 @@ mod imp {
                         "Calling Codes",
                         "List of country calling codes",
                         CallingCodes::static_type(),
-                        glib::ParamFlags::READWRITE | glib::ParamFlags::CONSTRUCT_ONLY,
+                        glib::ParamFlags::READABLE,
                     ),
                     glib::ParamSpecString::new(
                         "country-code",
                         "Country Code",
                         "A two-letter ISO 3166-1 alpha-2 country code",
                         None,
-                        glib::ParamFlags::READWRITE | glib::ParamFlags::CONSTRUCT_ONLY,
+                        glib::ParamFlags::READABLE,
                     ),
                     glib::ParamSpecString::new(
                         "name",
                         "Name",
                         "Native name of the country",
                         None,
-                        glib::ParamFlags::READWRITE | glib::ParamFlags::CONSTRUCT_ONLY,
+                        glib::ParamFlags::READABLE,
                     ),
                 ]
             });
             PROPERTIES.as_ref()
-        }
-
-        fn set_property(
-            &self,
-            _obj: &Self::Type,
-            _id: usize,
-            value: &glib::Value,
-            pspec: &glib::ParamSpec,
-        ) {
-            match pspec.name() {
-                "calling-codes" => self.calling_codes.set(value.get().unwrap()).unwrap(),
-                "country-code" => self
-                    .country_code
-                    .set(value.get::<Option<String>>().unwrap().unwrap_or_default())
-                    .unwrap(),
-                "name" => self
-                    .name
-                    .set(value.get::<Option<String>>().unwrap().unwrap_or_default())
-                    .unwrap(),
-                _ => unimplemented!(),
-            }
         }
 
         fn property(&self, obj: &Self::Type, _id: usize, pspec: &glib::ParamSpec) -> glib::Value {
@@ -120,33 +99,42 @@ impl From<types::CountryInfo> for CountryInfo {
     fn from(country_info: types::CountryInfo) -> Self {
         Self::new(
             BTreeSet::from_iter(country_info.calling_codes),
-            &country_info.country_code,
-            &country_info.name,
+            country_info.country_code,
+            country_info.name,
         )
     }
 }
 
 impl CountryInfo {
-    fn new(calling_codes: BTreeSet<String>, country_code: &str, name: &str) -> Self {
-        glib::Object::new(&[
-            ("calling-codes", &CallingCodes(calling_codes)),
-            ("country-code", &country_code),
-            ("name", &name),
-        ])
-        .expect("Failed to create CountryInfo")
+    fn new(calling_codes: BTreeSet<String>, country_code: String, name: String) -> Self {
+        let country_info: CountryInfo =
+            glib::Object::new(&[]).expect("Failed to create CountryInfo");
+        let imp = country_info.imp();
+
+        let calling_codes = CallingCodes(calling_codes);
+
+        imp.calling_codes.set(calling_codes).unwrap();
+        imp.country_code.set(country_code).unwrap();
+        imp.name.set(name).unwrap();
+
+        country_info
     }
 
     /// The invalid `CountryInfo` can be used when no valid calling code was specified by the user.
     pub(crate) fn invalid() -> Self {
-        Self::new(Default::default(), "", &gettext("Invalid Country Code"))
+        Self::new(
+            Default::default(),
+            "".into(),
+            gettext("Invalid Country Code"),
+        )
     }
 
     /// The test `CountryInfo` is used for Telegram test numbers and is set as `99966`.
     pub(crate) fn test() -> Self {
         Self::new(
             BTreeSet::from_iter(Some("99966".to_string())),
-            "",
-            &gettext("Test Account"),
+            "".into(),
+            gettext("Test Account"),
         )
     }
 
