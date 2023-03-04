00c9faacbd62c637da39c06a4abeda85855fdc3e
Marco Melorio <marco.melorio@protonmail.com>
2023-03-04 23:33:20 +0100

    indicators: Port to glib SignalGroup
    Let's make things more simple here too. It also fixes a warning about
    disconnecting a non-existent signal while scrolling.

--- src/session/content/message_row/indicators.rs
+++ src/session/content/message_row/indicators.rs
@@ -1,15 +1,17 @@
-use super::indicators_model::MessageIndicatorsModel;
-
+use gettextrs::gettext;
+use glib::clone;
 use gtk::prelude::*;
 use gtk::subclass::prelude::*;
 use gtk::{glib, CompositeTemplate};
+use tdlib::enums::MessageSendingState;
 
-use crate::tdlib::{ChatType, Message};
+use crate::tdlib::{Chat, ChatType, Message, MessageInteractionInfo, SponsoredMessage};
 
 mod imp {
     use super::*;
-    use glib::clone;
     use once_cell::sync::Lazy;
+    use once_cell::unsync::OnceCell;
+    use std::cell::RefCell;
 
     #[derive(Debug, Default, CompositeTemplate)]
     #[template(string = r#"
@@ -25,24 +27,24 @@ mod imp {
                 icon-name: "mail-reply-sender-symbolic";
             }
 
-            Label {
-                label: bind MessageIndicators.model.reply-count;
-            }
+            Label reply_count_label {}
         }
 
-        Label {
-            label: bind MessageIndicators.model.message-info;
-        }
-
-        Image sending_state_icon {
-            icon-name: bind MessageIndicators.model.sending-state-icon-name;
-        }
+        Label message_info_label {}
+        Image sending_state_icon {}
     }
     "#)]
     pub(crate) struct MessageIndicators {
-        pub(super) model: MessageIndicatorsModel,
+        pub(super) message: RefCell<Option<glib::Object>>,
+        pub(super) message_signal_group: OnceCell<glib::SignalGroup>,
+        pub(super) interaction_info_signal_group: OnceCell<glib::SignalGroup>,
+        pub(super) chat_signal_group: OnceCell<glib::SignalGroup>,
         #[template_child]
         pub(super) reply_count_box: TemplateChild<gtk::Box>,
+        #[template_child]
+        pub(super) reply_count_label: TemplateChild<gtk::Label>,
+        #[template_child]
+        pub(super) message_info_label: TemplateChild<gtk::Label>,
         #[template_child]
         pub(super) sending_state_icon: TemplateChild<gtk::Image>,
     }
@@ -66,14 +68,9 @@ mod imp {
     impl ObjectImpl for MessageIndicators {
         fn properties() -> &'static [glib::ParamSpec] {
             static PROPERTIES: Lazy<Vec<glib::ParamSpec>> = Lazy::new(|| {
-                vec![
-                    glib::ParamSpecObject::builder::<glib::Object>("message")
-                        .explicit_notify()
-                        .build(),
-                    glib::ParamSpecObject::builder::<MessageIndicatorsModel>("model")
-                        .read_only()
-                        .build(),
-                ]
+                vec![glib::ParamSpecObject::builder::<glib::Object>("message")
+                    .explicit_notify()
+                    .build()]
             });
             PROPERTIES.as_ref()
         }
@@ -92,7 +89,6 @@ mod imp {
 
             match pspec.name() {
                 "message" => obj.message().to_value(),
-                "model" => obj.model().to_value(),
                 _ => unimplemented!(),
             }
         }
@@ -100,32 +96,7 @@ mod imp {
         fn constructed(&self) {
             self.parent_constructed();
 
-            let obj = self.obj();
-
-            self.model.connect_notify_local(
-                Some("message"),
-                clone!(@weak obj => move |_, _| {
-                    obj.update_reply_count_visibility();
-                    obj.notify("message");
-                }),
-            );
-
-            self.model.connect_notify_local(
-                Some("reply-count"),
-                clone!(@weak obj => move |_, _| {
-                    obj.update_reply_count_visibility();
-                }),
-            );
-
-            self.sending_state_icon
-                .connect_icon_name_notify(|sending_state_icon| {
-                    sending_state_icon.set_visible(
-                        sending_state_icon
-                            .icon_name()
-                            .map(|icon_name| !icon_name.is_empty())
-                            .unwrap_or(false),
-                    )
-                });
+            self.obj().create_signal_groups();
         }
 
         fn dispose(&self) {
@@ -146,29 +117,168 @@ glib::wrapper! {
 }
 
 impl MessageIndicators {
+    fn create_signal_groups(&self) {
+        let imp = self.imp();
+
+        let message_signal_group = glib::SignalGroup::new(Message::static_type());
+        message_signal_group.connect_local(
+            "notify::is-edited",
+            false,
+            clone!(@weak self as obj => @default-return None, move |_| {
+                obj.update_message_info();
+                None
+            }),
+        );
+        imp.message_signal_group.set(message_signal_group).unwrap();
+
+        let interaction_info_signal_group =
+            glib::SignalGroup::new(MessageInteractionInfo::static_type());
+        interaction_info_signal_group.connect_local(
+            "notify::reply-count",
+            false,
+            clone!(@weak self as obj => @default-return None, move |_| {
+                obj.update_reply_count();
+                None
+            }),
+        );
+        imp.interaction_info_signal_group
+            .set(interaction_info_signal_group)
+            .unwrap();
+
+        let chat_signal_group = glib::SignalGroup::new(Chat::static_type());
+        chat_signal_group.connect_local(
+            "notify::last-read-outbox-message-id",
+            false,
+            clone!(@weak self as obj => @default-return None, move |_| {
+                obj.update_sending_state();
+                None
+            }),
+        );
+        imp.chat_signal_group.set(chat_signal_group).unwrap();
+    }
+
     pub(crate) fn message(&self) -> glib::Object {
-        self.imp().model.message()
+        self.imp().message.borrow().clone().unwrap()
     }
 
     pub(crate) fn set_message(&self, message: glib::Object) {
-        self.imp().model.set_message(message);
+        let imp = self.imp();
+        let old = imp.message.replace(Some(message));
+
+        if old == *imp.message.borrow() {
+            return;
+        }
+
+        let maybe_message_ref = imp.message.borrow();
+        let maybe_message = maybe_message_ref.and_downcast_ref::<Message>();
+
+        imp.message_signal_group
+            .get()
+            .unwrap()
+            .set_target(maybe_message);
+        imp.interaction_info_signal_group
+            .get()
+            .unwrap()
+            .set_target(maybe_message.map(|m| m.interaction_info()));
+        imp.chat_signal_group
+            .get()
+            .unwrap()
+            .set_target(maybe_message.map(|m| m.chat()).as_ref());
+
+        self.update_reply_count();
+        self.update_sending_state();
+        self.update_message_info();
+
+        self.notify("message");
     }
 
-    fn update_reply_count_visibility(&self) {
-        let is_channel_message = self.message()
-            .downcast::<Message>()
-            .ok()
+    fn update_reply_count(&self) {
+        let imp = self.imp();
+
+        let maybe_message_ref = imp.message.borrow();
+        let maybe_message = maybe_message_ref.and_downcast_ref::<Message>();
+
+        let is_channel_message = maybe_message
             .filter(|message| {
                 matches!(message.chat().type_(), ChatType::Supergroup(data) if data.is_channel())
             })
             .is_some();
 
-        self.imp()
-            .reply_count_box
-            .set_visible(!is_channel_message && self.model().reply_count() > 0);
+        if is_channel_message {
+            imp.reply_count_label.set_label("");
+            imp.reply_count_box.set_visible(false);
+        } else {
+            let reply_count = maybe_message
+                .map(Message::interaction_info)
+                .map(MessageInteractionInfo::reply_count)
+                .unwrap_or(0);
+
+            if reply_count > 0 {
+                imp.reply_count_label.set_label(&reply_count.to_string());
+                imp.reply_count_box.set_visible(true);
+            } else {
+                imp.reply_count_label.set_label("");
+                imp.reply_count_box.set_visible(false);
+            }
+        }
     }
 
-    pub(crate) fn model(&self) -> &MessageIndicatorsModel {
-        &self.imp().model
+    fn update_sending_state(&self) {
+        let imp = self.imp();
+        let maybe_icon_name = imp
+            .message
+            .borrow()
+            .and_downcast_ref::<Message>()
+            .filter(|message| message.is_outgoing())
+            .map(|message| match message.sending_state() {
+                Some(state) => match state.0 {
+                    MessageSendingState::Failed(_) => "message-failed-symbolic",
+                    MessageSendingState::Pending => "message-pending-symbolic",
+                },
+                None => {
+                    if message.chat().is_own_chat()
+                        || message.id() <= message.chat().last_read_outbox_message_id()
+                    {
+                        "message-read-symbolic"
+                    } else {
+                        "message-unread-left-symbolic"
+                    }
+                }
+            });
+
+        if let Some(icon_name) = maybe_icon_name {
+            imp.sending_state_icon.set_icon_name(Some(icon_name));
+            imp.sending_state_icon.set_visible(true);
+        } else {
+            imp.sending_state_icon.set_icon_name(None);
+            imp.sending_state_icon.set_visible(false);
+        }
+    }
+
+    fn update_message_info(&self) {
+        let imp = self.imp();
+        let message = imp.message.borrow();
+
+        let label = if let Some(message) = message.and_downcast_ref::<Message>() {
+            let datetime = glib::DateTime::from_unix_utc(message.date() as i64)
+                .and_then(|t| t.to_local())
+                // Translators: This is a time representation, without seconds.
+                // Here you may want to change to a 24-hours representation, based on your locale.
+                // You can use this site to learn more: https://www.strfti.me/
+                .and_then(|t| t.format(&gettext("%l:%M %p")))
+                .unwrap();
+
+            if message.is_edited() {
+                format!("{} {}", gettext("edited"), datetime)
+            } else {
+                datetime.into()
+            }
+        } else if message.and_downcast_ref::<SponsoredMessage>().is_some() {
+            gettext("sponsored")
+        } else {
+            unreachable!()
+        };
+
+        imp.message_info_label.set_label(&label);
     }
 }
