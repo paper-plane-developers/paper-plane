8edc893990592cb6509471fd83ffc3e64d891981
Karol Lademan <karol@lademan.dev>
2023-06-07 18:28:30 +0200

    refactor(avatar): Port Avatar logic to SignalGroup
    Co-authored-by: Marcus Behrendt <marcus.behrendt.86@gmail.com>

--- src/components/avatar.rs
+++ src/components/avatar.rs
@@ -1,15 +1,15 @@
+use once_cell::unsync::OnceCell;
 use std::cell::RefCell;
 
 use adw::subclass::prelude::BinImpl;
 use glib::clone;
-use glib::closure;
 use gtk::gdk;
 use gtk::glib;
 use gtk::prelude::*;
 use gtk::subclass::prelude::*;
 use gtk::CompositeTemplate;
 
-use crate::expressions;
+use crate::strings;
 use crate::tdlib::Avatar as AvatarItem;
 use crate::tdlib::Chat;
 use crate::tdlib::User;
@@ -24,7 +24,8 @@ mod imp {
     pub(crate) struct Avatar {
         /// A `Chat` or `User`
         pub(super) item: RefCell<Option<glib::Object>>,
-        pub(super) handler_id: RefCell<Option<glib::SignalHandlerId>>,
+        pub(super) user_signal_group: OnceCell<glib::SignalGroup>,
+        pub(super) chat_signal_group: OnceCell<glib::SignalGroup>,
         #[template_child]
         pub(super) avatar: TemplateChild<adw::Avatar>,
     }
@@ -88,7 +89,7 @@ mod imp {
 
         fn constructed(&self) {
             self.parent_constructed();
-            self.obj().setup_expressions();
+            self.obj().create_signal_groups();
         }
     }
 
@@ -112,49 +113,52 @@ impl Avatar {
         glib::Object::new()
     }
 
-    fn setup_expressions(&self) {
-        let item_expression = Self::this_expression("item");
+    fn create_signal_groups(&self) {
         let imp = self.imp();
 
-        // Chat title expression
-        let title_expression = expressions::chat_display_name(&item_expression);
-        gtk::ClosureExpression::new::<String>(
-            &[item_expression.clone().upcast(), title_expression],
-            closure!(|_: Self, item: Option<glib::Object>, title: String| {
-                item.as_ref()
-                    .and_then(|i| i.downcast_ref::<Chat>())
-                    .filter(|chat| chat.is_own_chat())
-                    // Workaround for having a blue AdwAvatar for Saved Messages chat
-                    .map(|_| "-".to_string())
-                    .unwrap_or(title)
+        let user_signal_group = glib::SignalGroup::new(User::static_type());
+        user_signal_group.connect_notify_local(
+            Some("type"),
+            clone!(@weak self as obj => move |_, _| {
+                obj.update_display_name();
+                obj.update_avatar();
             }),
-        )
-        .upcast()
-        .bind(&*imp.avatar, "text", Some(self));
+        );
+        user_signal_group.connect_notify_local(
+            Some("first-name"),
+            clone!(@weak self as obj => move |_, _| {
+                obj.update_display_name();
+            }),
+        );
+        user_signal_group.connect_notify_local(
+            Some("last-name"),
+            clone!(@weak self as obj => move |_, _| {
+                obj.update_display_name();
+            }),
+        );
+        user_signal_group.connect_notify_local(
+            Some("avatar"),
+            clone!(@weak self as obj => move|_, _| {
+                obj.update_avatar();
+            }),
+        );
+        imp.user_signal_group.set(user_signal_group).unwrap();
 
-        // User title expression
-        expressions::user_display_name(&item_expression).bind(&*imp.avatar, "text", Some(self));
+        let chat_signal_group = glib::SignalGroup::new(Chat::static_type());
+        chat_signal_group.connect_notify_local(
+            Some("title"),
+            clone!(@weak self as obj => move |_, _| {
+                obj.update_display_name()
+            }),
+        );
+        chat_signal_group.connect_notify_local(
+            Some("avatar"),
+            clone!(@weak self as obj => move|_, _| {
+                obj.update_avatar();
+            }),
+        );
 
-        // Icon expression
-        let icon_name_expression =
-            item_expression.chain_closure::<Option<String>>(closure!(|_: Self,
-                                                                      item: Option<
-                glib::Object,
-            >| {
-                item.as_ref()
-                    .and_then(|i| i.downcast_ref::<Chat>())
-                    .filter(|chat| chat.is_own_chat())
-                    // Show bookmark icon for Saved Messages chat
-                    .map(|_| "user-bookmarks-symbolic")
-            }));
-        icon_name_expression.bind(&*imp.avatar, "icon-name", Some(self));
-
-        // Don't show the initials if we show a custom icon
-        icon_name_expression
-            .chain_closure::<bool>(closure!(
-                |_: Self, icon_name: Option<String>| icon_name.is_none()
-            ))
-            .bind(&*imp.avatar, "show-initials", Some(self));
+        imp.chat_signal_group.set(chat_signal_group).unwrap();
     }
 
     fn load_image(&self, avatar_item: Option<AvatarItem>, session: Session) {
@@ -172,6 +176,44 @@ impl Avatar {
             }
         } else {
             self.imp().avatar.set_custom_image(gdk::Paintable::NONE);
+        }
+    }
+
+    fn update_avatar(&self) {
+        let imp = self.imp();
+
+        imp.avatar.set_custom_image(gdk::Paintable::NONE);
+        imp.avatar.set_icon_name(None);
+        imp.avatar.set_show_initials(true);
+
+        if let Some(item) = self.item() {
+            if let Some(user) = item.downcast_ref::<User>() {
+                self.load_image(user.avatar(), user.session());
+            } else if let Some(chat) = item.downcast_ref::<Chat>() {
+                if chat.is_own_chat() {
+                    imp.avatar.set_icon_name(Some("user-bookmarks-symbolic"));
+                    imp.avatar.set_show_initials(false);
+                } else {
+                    self.load_image(chat.avatar(), chat.session());
+                }
+            }
+        }
+    }
+
+    fn update_display_name(&self) {
+        let imp = self.imp();
+
+        if let Some(item) = self.item() {
+            if let Some(user) = item.downcast_ref::<User>() {
+                imp.avatar
+                    .set_text(Some(&strings::user_display_name(user, true)));
+            } else if let Some(chat) = item.downcast_ref::<Chat>() {
+                if chat.is_own_chat() {
+                    imp.avatar.set_text(Some("-"));
+                } else {
+                    imp.avatar.set_text(Some(chat.title().as_ref()));
+                };
+            }
         }
     }
 
@@ -194,37 +236,20 @@ impl Avatar {
     pub(crate) fn set_item(&self, item: Option<glib::Object>) {
         let imp = self.imp();
 
-        if let Some(id) = imp.handler_id.take() {
-            self.item().unwrap().disconnect(id);
-        }
-
-        if let Some(ref item) = item {
-            if let Some(chat) = item.downcast_ref::<Chat>() {
-                if chat.is_own_chat() {
-                    imp.avatar.set_custom_image(gdk::Paintable::NONE);
-                } else {
-                    self.load_image(chat.avatar(), chat.session());
-                    let handler_id =
-                        chat.connect_avatar_notify(clone!(@weak self as obj => move |chat, _| {
-                            obj.load_image(chat.avatar(), chat.session());
-                        }));
-                    imp.handler_id.replace(Some(handler_id));
-                }
-            } else if let Some(user) = item.downcast_ref::<User>() {
-                self.load_image(user.avatar(), user.session());
-                let handler_id =
-                    user.connect_avatar_notify(clone!(@weak self as obj => move |user, _| {
-                        obj.load_image(user.avatar(), user.session());
-                    }));
-                imp.handler_id.replace(Some(handler_id));
-            } else {
-                imp.avatar.set_custom_image(gdk::Paintable::NONE);
-            }
-        } else {
-            imp.avatar.set_custom_image(gdk::Paintable::NONE);
-        }
+        imp.chat_signal_group
+            .get()
+            .unwrap()
+            .set_target(item.as_ref().and_then(|item| item.downcast_ref::<Chat>()));
+        imp.user_signal_group
+            .get()
+            .unwrap()
+            .set_target(item.as_ref().and_then(|item| item.downcast_ref::<User>()));
 
         imp.item.replace(item);
+
+        self.update_display_name();
+        self.update_avatar();
+
         self.notify("item");
     }
 
