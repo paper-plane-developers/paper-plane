54cd4238e9c6cfe198cf611cd74284547acffddf
Marcus Behrendt <marcus.behrendt.86@gmail.com>
2023-10-05 23:12:05 +0200

    feat(ui/message-location): Open in window on click

--- src/ui/session/content/message_row/location.rs
+++ src/ui/session/content/message_row/location.rs
@@ -24,6 +24,7 @@ mod imp {
         pub(super) message: glib::WeakRef<model::Message>,
         pub(super) handler_id: RefCell<Option<glib::SignalHandlerId>>,
         pub(super) expire_source_id: RefCell<Option<glib::SourceId>>,
+        pub(super) map_window: glib::WeakRef<ui::MapWindow>,
         #[template_child]
         pub(super) message_bubble: TemplateChild<ui::MessageBubble>,
         #[template_child]
@@ -46,6 +47,8 @@ mod imp {
 
         fn class_init(klass: &mut Self::Class) {
             Self::bind_template(klass);
+            Self::bind_template_callbacks(klass);
+
             klass.set_css_name("messagelocation");
 
             klass.install_action("message-row.open", None, move |widget, _, _| {
@@ -85,6 +88,25 @@ mod imp {
 
     impl WidgetImpl for MessageLocation {}
     impl ui::MessageBaseImpl for MessageLocation {}
+
+    #[gtk::template_callbacks]
+    impl MessageLocation {
+        #[template_callback]
+        fn on_map_gesture_click_pressed(&self) {
+            let obj = &*self.obj();
+
+            let map_window =
+                ui::MapWindow::new(self.obj().root().and_downcast_ref::<gtk::Window>());
+            map_window.add_css_class("location");
+
+            self.map_window.set(Some(&map_window));
+
+            obj.update_map_window(&self.message.upgrade().unwrap());
+            map_window.center_marker();
+
+            map_window.present();
+        }
+    }
 }
 
 glib::wrapper! {
@@ -113,10 +135,11 @@ impl MessageBaseExt for MessageLocation {
         // Update the message.
         let handler_id =
             message.connect_content_notify(clone!(@weak self as obj => move |message| {
-                obj.update(message);
+                obj.update_row(message);
+                obj.update_map_window(message);
             }));
         imp.handler_id.replace(Some(handler_id));
-        self.update(message);
+        self.update_row(message);
 
         imp.message.set(Some(message));
         self.notify("message");
@@ -133,7 +156,7 @@ impl MessageLocation {
         .launch(gtk::Window::NONE, gio::Cancellable::NONE, |_| {});
     }
 
-    fn update(&self, message: &model::Message) {
+    fn update_row(&self, message: &model::Message) {
         if let tdlib::enums::MessageContent::MessageLocation(message_) = message.content().0 {
             let imp = self.imp();
 
@@ -215,6 +238,21 @@ impl MessageLocation {
             Some(now)
         } else {
             None
+        }
+    }
+
+    fn update_map_window(&self, message: &model::Message) {
+        if let Some(map_window) = self.imp().map_window.upgrade() {
+            if let tdlib::enums::MessageContent::MessageLocation(message_) = message.content().0 {
+                let map = map_window.map();
+
+                if message_.live_period > 0 {
+                    map.set_custom_marker(Some(
+                        ui::AvatarMapMarker::from(message.sender().as_user().unwrap()).upcast(),
+                    ));
+                }
+                map.set_marker_position(message_.location.latitude, message_.location.longitude);
+            }
         }
     }
 }
